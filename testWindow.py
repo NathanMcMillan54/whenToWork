941898e6f4e08401f1fc55d16a47d7009b598f14
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-15 22:38:57 -0400

    Made an input button.

--- testWindow.py
+++ testWindow.py
@@ -8,6 +8,18 @@ L = Label(root, text="whenToWork")
 L.config(font=("Courier", 14))
 L.pack()
 
+
 # add stuff to make input
+
+def getInput():
+    project = inputBox.get("1.0", "end-1c")
+    print(project)
+
+
+inputBox = Text(root, height=2, width=10)
+inputBox.pack()
+buttonCommit = Button(root, height=1, width=10, text="Enter", command=lambda: getInput())
+# command=lambda: retrieve_input() >>> just means do this when i press the button
+buttonCommit.pack()
 
 root.mainloop()
