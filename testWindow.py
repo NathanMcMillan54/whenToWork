59ad98adf36515fc56a122a54a3befcf82fec72d
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-15 22:31:48 -0400

    Started making GUI for this.

---
+++ testWindow.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.title("whenToWork")
+root.configure(background="#ffffff")
+
+L = Label(root, text="whenToWork")
+L.config(font=("Courier", 14))
+L.pack()
+
+# add stuff to make input
+
+root.mainloop()
