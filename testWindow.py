0b1f233246e7514349b05a8c8b0bbe850b858cab
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-09-16 11:11:58 -0400

    Added all the timer stuff from main.py into testWindow.py.

--- testWindow.py
+++ testWindow.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import time
+import os
 
 root = Tk()
 root.title("whenToWork")
@@ -16,8 +18,41 @@ def getProjectInput():
 
 
 def getProjectValueInput():
-    projectValue = inputProjectValueBox.get("1.0", "end-1c")
+    projectValue = int(inputProjectValueBox.get("1.0", "end-1c"))
     print(projectValue)
+
+
+def inputText():
+    # get input from both input boxes
+    projectValue = int(1)
+
+    if projectValue > 7:
+        print("Work for 60 minutes")
+        time.sleep(3600)
+        os.system('python whenToWork/stop.py')
+        print("Stop")
+        exit()
+    elif projectValue < 3:
+        print("Work for 15 minutes")
+        time.sleep(900)
+        os.system('python whenToWork/stop.py')
+        print("Stop")
+        exit()
+    elif projectValue == 4:
+        print("Work for 30 minutes")
+        time.sleep(1800)
+        os.system('python whenToWork/stop.py')
+        exit()
+    elif projectValue == 5:
+        print("Work for 45 minutes")
+        time.sleep(2700)
+        os.system('python whenToWork/stop.py')
+        exit()
+    elif projectValue == 6:
+        print("Work for 45 minutes")
+        time.sleep(2700)
+        os.system('python whenToWork/stop.py')
+        exit()
 
 
 PT = Label(root, text="Project Name")
@@ -26,8 +61,8 @@ PT.pack()
 
 inputProjectBox = Text(root, height=2, width=10)
 inputProjectBox.pack()
+# soon figure out how to only have 1 input button and 2 input boxes
 buttonEnterProject = Button(root, height=1, width=10, text="Enter", command=lambda: getProjectInput())
-
 buttonEnterProject.pack()
 
 PVT = Label(root, text="Input 1-10")
